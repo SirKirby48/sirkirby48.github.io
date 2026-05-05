fc5b4a0246b2c9bcf534389ecd7bff44cfc413b7
SirKirby49 <sir.kirby49@gmail.com>
2026-05-05 10:38:58 -0400

    Updates
    polishing 1 of idk

--- script1.js
+++ script1.js
@@ -30,10 +30,34 @@ console.log(stat1);
 
 }
 
+function oneadd15() { let stat1 = Number(FirstINT.value);
+console.log(stat1);
+stat1 = stat1 + 5;
+console.log("+5");
+console.log(stat1);
+document.getElementById("FirstResult").innerHTML = stat1;
+FirstINT.value = stat1;
+console.log(FirstINT.value);
+console.log(stat1);
+
+}
+
 function oneminus1() { let stat1 = Number(FirstINT.value);
 console.log(stat1);
 stat1 = stat1 - 1;
 console.log("-1");
+console.log(stat1);
+document.getElementById("FirstResult").innerHTML = stat1;
+FirstINT.value = stat1;
+console.log(FirstINT.value);
+console.log(stat1);
+
+}
+
+function oneminus15() { let stat1 = Number(FirstINT.value);
+console.log(stat1);
+stat1 = stat1 - 5;
+console.log("-5");
 console.log(stat1);
 document.getElementById("FirstResult").innerHTML = stat1;
 FirstINT.value = stat1;
@@ -61,10 +85,34 @@ console.log(stat2);
 
 }
 
+function oneadd25() {  let stat2 = Number(SecondINT.value);
+console.log(stat2);
+stat2 = stat2 + 5;
+console.log("+5");
+console.log(stat2);
+document.getElementById("SecondResult").innerHTML = stat2;
+SecondINT.value = stat2;
+console.log(SecondINT.value);
+console.log(stat2);
+
+}
+
 function oneminus2() { let stat2 = Number(SecondINT.value);
 console.log(stat2);
 stat2 = stat2 - 1;
 console.log("-1");
+console.log(stat2);
+document.getElementById("SecondResult").innerHTML = stat2;
+SecondINT.value = stat2;
+console.log(SecondINT.value);
+console.log(stat2);
+
+}
+
+function oneminus25() { let stat2 = Number(SecondINT.value);
+console.log(stat2);
+stat2 = stat2 - 5;
+console.log("-5");
 console.log(stat2);
 document.getElementById("SecondResult").innerHTML = stat2;
 SecondINT.value = stat2;
@@ -92,10 +140,34 @@ console.log(stat3);
 
 }
 
+function oneadd35() {  let stat3 = Number(ThirdINT.value);
+console.log(stat3);
+stat3 = stat3 + 5;
+console.log("+5");
+console.log(stat3);
+document.getElementById("ThirdResult").innerHTML = stat3;
+ThirdINT.value = stat3;
+console.log(ThirdINT.value);
+console.log(stat3);
+
+}
+
 function oneminus3() { let stat3 = Number(ThirdINT.value);
 console.log(stat3);
 stat3 = stat3 - 1;
 console.log("-1");
+console.log(stat3);
+document.getElementById("ThirdResult").innerHTML = stat3;
+ThirdINT.value = stat3;
+console.log(ThirdINT.value);
+console.log(stat3);
+
+}
+
+function oneminus35() { let stat3 = Number(ThirdINT.value);
+console.log(stat3);
+stat3 = stat3 - 5;
+console.log("-5");
 console.log(stat3);
 document.getElementById("ThirdResult").innerHTML = stat3;
 ThirdINT.value = stat3;
