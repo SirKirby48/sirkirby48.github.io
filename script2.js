860688487a59cbc282947d6352b6a5747e2996b6
SirKirby49 <sir.kirby49@gmail.com>
2026-05-11 10:20:17 -0400

    polishing

--- script2.js
+++ script2.js
@@ -6,10 +6,10 @@ function loadConstructEmbed() { const loader1 = <iframe src="echoChambers/index.
 
 function loadItchEmbed() {// let loader2 = <iframe frameborder="0" src="https://itch.io/embed-upload/16869772?color=111a43" allow="fullscreen" width="640" height="380"><a href="https://sir-kirby.itch.io/echo-chambers">Play Echo Chambers on itch.io</a></iframe>
 //document.getElementById("div2").innerHTML = defineProperties(loader2);
-var iframeCreator2 = document.createElement("iframe2")
-iframeCreator2.setAttribute("src","https://itch.io/embed/3944305?linkback=true&amp;bg_color=1e39a1&amp;fg_color=43e5fa&amp;link_color=63cb9b&amp;border_color=3e4770");
-iframeCreator2.setAttribute("width","400px");
-iframeCreator2.setAttribute("height","200px");
-iframeCreator2.setAttribute("name","Echo Chambers by Sir_Kirby!")
-document.body.appendChild(iframeCreator2)
+let x = document.createElement("IFRAME");
+x.setAttribute("src","https://itch.io/embed/3944305?linkback=true&amp;bg_color=1e39a1&amp;fg_color=43e5fa&amp;link_color=63cb9b&amp;border_color=3e4770");
+// x.setAttribute("width","400px");
+// x.setAttribute("height","200px");
+// x.setAttribute("name","Echo Chambers by Sir_Kirby!");
+document.body.appendChild(x);
 }
