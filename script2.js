87d4401cb4aec864be8cdf9bbec5e50745ccb5ec
SirKirby49 <sir.kirby49@gmail.com>
2026-05-11 09:46:33 -0400

    attempted polishing 2
    Attempting to load iframes with MouseEvent:button onclick()

--- script2.js
+++ script2.js
@@ -1,8 +1,15 @@
-function loadConstructEmbed() { let loader1 = <iframe src="echoChambers/index.html" loading="lazy" allow="fullscreen" width="500" height="260"></iframe>;
-document.getElementById("div1").innerHTML.valueOf = loader1;
-
+function loadConstructEmbed() { const loader1 = <iframe src="echoChambers/index.html" allow="fullscreen" width="500" height="260"></iframe>;
+// const div = document.getElementById("button10")
+// document.getElementById("button10").innerHTML = appendChild(loader1);
+// div.append(loader1)
 }
 
-function loadItchEmbed() { let loader2 = <iframe frameborder="0" src="https://itch.io/embed-upload/16869772?color=111a43" loading="lazy" allow="fullscreen" width="640" height="380"><a href="https://sir-kirby.itch.io/echo-chambers">Play Echo Chambers on itch.io</a></iframe>
-document.getElementById("div2").innerHTML = defineProperties(loader2);
+function loadItchEmbed() {// let loader2 = <iframe frameborder="0" src="https://itch.io/embed-upload/16869772?color=111a43" allow="fullscreen" width="640" height="380"><a href="https://sir-kirby.itch.io/echo-chambers">Play Echo Chambers on itch.io</a></iframe>
+//document.getElementById("div2").innerHTML = defineProperties(loader2);
+var iframeCreator2 = document.createElement("iframe2")
+iframeCreator2.setAttribute("src","https://itch.io/embed/3944305?linkback=true&amp;bg_color=1e39a1&amp;fg_color=43e5fa&amp;link_color=63cb9b&amp;border_color=3e4770");
+iframeCreator2.setAttribute("width","400px");
+iframeCreator2.setAttribute("height","200px");
+iframeCreator2.setAttribute("name","Echo Chambers by Sir_Kirby!")
+document.body.appendChild(iframeCreator2)
 }
